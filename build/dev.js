4607413d4b62f1629a4218a5995c3bd8066dd510
Yuecui Li <lyc.dream@163.com>
2017-06-26 15:57:53 +0800

    use webpack-dev-server to start the server.

--- build/dev.js
+++ build/dev.js
@@ -1,6 +1,21 @@
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var opn = require('opn')
+var devConfig = require('./webpack.config')
 
-const webpack = require('webpack')
+devConfig.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
 
+var compiler = webpack(devConfig);
+var server = new WebpackDevServer(compiler, {
+    contentBase:'dist/'
+});
 
-webpack()
-
+server.listen(8080, err => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  const uri = 'http://localhost:8080'
+  console.log('Listening at ' + uri + '\n')
+  opn(uri)
+})
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ var Vue = require('vue');
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue, ye!'
   }
 })
