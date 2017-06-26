004ab35da216d4ca309be6f55f2ac47404dde3b3
Yuecui Li <lyc.dream@163.com>
2017-06-26 17:21:32 +0800

    do not need contentBase since using html-loader.

--- build/dev.js
+++ build/dev.js
@@ -6,9 +6,7 @@ var devConfig = require('./webpack.config')
 devConfig.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
 
 var compiler = webpack(devConfig);
-var server = new WebpackDevServer(compiler, {
-    contentBase:'dist/'
-});
+var server = new WebpackDevServer(compiler);
 
 server.listen(8080, err => {
   if (err) {
